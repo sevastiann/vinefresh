51e5b21004520a3091a74f8b6da37297ce138a51
ling <linggrr@gmail.com>
2025-11-28 20:17:04 -0500

    Tratando de llevar a cabo la portabilidad pc/movil

--- manage.py
+++ manage.py
@@ -2,19 +2,26 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
+
+    # 🔥 Cargar el archivo .env ANTES de leer settings
+    load_dotenv()
+
+    # 🔥 Configurar módulo de settings
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinefresh.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Make sure it's installed and "
+            "your virtual environment is activated."
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
